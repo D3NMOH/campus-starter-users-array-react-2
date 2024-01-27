ed0e2abbb7c090a395c6d39aefda1856b029f448
D3NMOH <d3nmoh@gmail.com>
2024-01-27 20:59:13 +0100

    gemacht 3

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,28 @@ function App() {
   const fullName = (user) => {
     return `${user.name.first} ${user.name.last}`;
   };
+  const surName = (usersurname) => {
+    return `${usersurname.name.last}`;
+  };
+  const country = (country) => {
+    return `${country.location.country}`;
+  };
 
   const sortedAndFilteredUsers = () => {
     let sortedUsersArray;
 
-    if (sort === "by-name") {
+    if (sort === "by-country") {
+      sortedUsersArray = users
+        .slice()
+        .sort((a, b) => country(a).localeCompare(country(b)));
+    } else if (sort === "by-name") {
       sortedUsersArray = users
         .slice()
         .sort((a, b) => fullName(a).localeCompare(fullName(b)));
+    } else if (sort === "by-surname") {
+      sortedUsersArray = users
+        .slice()
+        .sort((a, b) => surName(a).localeCompare(surName(b)));
     } else if (sort === "by-age") {
       sortedUsersArray = users.slice().sort((a, b) => a.dob.age - b.dob.age);
     } else {
@@ -46,41 +60,64 @@ function App() {
       </header>
       <div className="buttons-block">
         <div className="sort">
-          <h2>Sort</h2>
+          <h2>
+            <i class="fa-solid fa-arrow-down-short-wide"> </i> Sort by
+          </h2>
           <button
             onClick={() => handleSort("by-name")}
             className={`action-button ${
               sort === "by-name" ? "button-highlight" : ""
             }`}
           >
-            By name
+            <i class="fa-solid fa-address-card"> </i> Name
           </button>
+          <button
+            onClick={() => handleSort("by-surname")}
+            className={`action-button ${
+              sort === "by-surname" ? "button-highlight" : ""
+            }`}
+          >
+            <i class="fa-regular fa-address-card"> </i> Surname
+          </button>
+          <br />
           <button
             onClick={() => handleSort("by-age")}
             className={`action-button ${
               sort === "by-age" ? "button-highlight" : ""
             }`}
           >
-            By age
+            <i class="fa-solid fa-person-cane"> </i> Age
           </button>
-        </div>{" "}
+          <button
+            onClick={() => handleSort("by-country")}
+            className={`action-button ${
+              sort === "by-country" ? "button-highlight" : ""
+            }`}
+          >
+            <i class="fa-solid fa-earth-europe"> </i> Country
+          </button>
+        </div>
         <div className="filter">
-          <h2>Filter</h2>
+          <h2>
+            <i class="fa-solid fa-filter"> </i> Filter
+          </h2>
+
           <button
             onClick={() => handleFilter("all")}
             className={`action-button ${
               filter === "all" ? "button-highlight" : ""
             }`}
           >
-            All
+            <i class="fa-solid fa-venus-mars"> </i> All
           </button>
+          <br />
           <button
             onClick={() => handleFilter("male")}
             className={`action-button ${
               filter === "male" ? "button-highlight" : ""
             }`}
           >
-            Men
+            <i class="fa-solid fa-mars"> </i> Men
           </button>
           <button
             onClick={() => handleFilter("female")}
@@ -88,7 +125,7 @@ function App() {
               filter === "female" ? "button-highlight" : ""
             }`}
           >
-            Women
+            <i class="fa-solid fa-venus"> </i> Women
           </button>
         </div>
       </div>
